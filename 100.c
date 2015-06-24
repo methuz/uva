5245a79b4960590e09a5a02d6ab66f1f208a81a7
methuz <methuz.k@gmail.com>
2015-06-24 12:29:28 +0700

    #100: Add swap input order handler and use unsigned int. Accepted

--- 100.c
+++ 100.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
-int max_cycle(int n);
+unsigned int max_cycle(unsigned int n);
 int main() {
-    int l, u, i, j;
-    int r[1000001] = {0};
+    unsigned int l, u, i, j;
+    unsigned int _l, _u;
+    unsigned int r[1000001] = {0};
     for (j = 1; j <= 1000000 ; j++) {
         r[j] = max_cycle(j);
     }
     while (scanf("%d %d", &l, &u) != EOF) {
-        int max = 0;
+        _l = l;
+        _u = u;
+        if (l > u) {
+            unsigned int v = l;
+            l = u;
+            u = v;
+        }
+        unsigned int max = 0;
         for (i = l; i <= u ; i++) {
             if (r[i] > max)
                 max = r[i];
         }
 
-        printf("%d %d %d\n", l, u, max);
+        printf("%d %d %d\n", _l, _u, max);
     }
     
 }
 
-int max_cycle(int n) {
-    int count = 1;
+unsigned int max_cycle(unsigned int n) {
+    unsigned int count = 1;
     while (n > 1) {
         count += 1;
         if (n % 2 == 0) {
